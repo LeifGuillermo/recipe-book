b73fb7e666d7defb5d86d6bcfc417617c519e5f8
Leif Guillermo <guillermoleif@gmail.com>
2018-12-21 09:30:13 -0700

    Adding in basic recipe. Need to add a loop to loop over recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Recipe } from './../recipe.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test.', 'https://www.maxpixel.net/photo-1459693')
+  ];
+
   constructor () {
+
+  }
+
+  ngOnInit() {
 
   }
 }
