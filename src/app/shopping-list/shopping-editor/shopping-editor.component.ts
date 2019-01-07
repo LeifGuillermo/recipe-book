0b9a2cac914c7aca5502a734997c297d9e67e80e
Leif Guillermo <guillermoleif@gmail.com>
2019-01-07 13:26:59 -0700

    Adding functionality to shopping list for adding items and clearing the list

--- src/app/shopping-list/shopping-editor/shopping-editor.component.ts
+++ src/app/shopping-list/shopping-editor/shopping-editor.component.ts
@@ -1,12 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild, EventEmitter, Output, ElementRef } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-editor',
   templateUrl: './shopping-editor.component.html'
 })
-
 export class ShoppingListEditorComponent {
-  constructor () {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
 
+  @Output() shoppingListItemAdded = new EventEmitter<Ingredient>();
+  @Output() shoppingListItemDeleted = new EventEmitter<void>();
+  @Output() shoppingListCleared = new EventEmitter<void>();
+
+  onAddItem() {
+    this.shoppingListItemAdded.emit(
+      new Ingredient(
+        this.nameInputRef.nativeElement.value,
+        this.amountInputRef.nativeElement.value
+      )
+    );
   }
+
+  onDelete() {}
+
+  onClear() {
+    this.shoppingListCleared.emit();
+  }
+
+  constructor() {}
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -6,16 +6,21 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
   ingredients: Ingredient[] = [
     new Ingredient('lbs of Rice', 5),
     new Ingredient('Shrimp', 100),
     new Ingredient('lbs of Pork', 5)
   ];
 
-  constructor() { }
-
-  ngOnInit() {
+  addShoppingListElement(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
   }
 
+  clearShoppingList() {
+    this.ingredients = [];
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
 }
