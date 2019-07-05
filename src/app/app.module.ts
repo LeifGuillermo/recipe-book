c2aad275c84fe5d2e8e37667ef091eb66145232f
Leif Guillermo <guillermoleif@gmail.com>
2019-07-04 23:42:57 -0600

    Add some initial routing to the application and cursor styles as well.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,38 @@
-import { RecipeService } from './recipes/services/recipe.service';
-import { ShoppingListService } from './shopping-list/services/shopping-list.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { DropdownDirective } from './shared/dropdown.directive';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
+import { NgModule } from '@angular/core';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeService } from './recipes/services/recipe.service';
+import { RouterModule, Routes } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditorComponent } from './shopping-list/shopping-editor/shopping-editor.component';
+import { ShoppingListService } from './shopping-list/services/shopping-list.service';
 
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { DropdownDirective } from './shared/dropdown.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-
     ShoppingListComponent,
     ShoppingListEditorComponent,
-
     RecipesComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
     RecipeListComponent,
-
     DropdownDirective
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
